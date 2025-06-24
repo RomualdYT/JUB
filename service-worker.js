c8bfb82e5bc84e9c4ea94731010c5769993d110d
RomualdYT <romuald@gameurnews.fr>
2025-06-24 21:49:34 +0200

    Handle caching errors

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,14 @@ const ASSETS = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(async cache => {
+      try {
+        // CDN assets might not be available offline, but this should not block activation
+        await cache.addAll(ASSETS);
+      } catch (err) {
+        console.warn('Failed to cache some assets', err);
+      }
+    })
   );
 });
 
